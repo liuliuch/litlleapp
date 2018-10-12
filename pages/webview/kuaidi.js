a5e2e139b70550132e5552323687179cc1782fad
liuliuch <1291823196@qq.com>
2018-10-12 17:34:38 +0800

    index
    index up

--- pages/webview/kuaidi.js
+++ pages/webview/kuaidi.js
@@ -6,7 +6,11 @@ Page({
       { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
       { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
       { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
-    ]
+    ],
+    circular:true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
   onLoad: function () {
   }
